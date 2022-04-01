cb9824754fd91c442031065145ddacdcb3d6acdd
Luis Antolin <luis.antolin@gmail.com>
2022-04-01 14:00:41 +0200

    corregidos problemas de conexion a un mariadb remoto

--- EjemploJdbc/src/main/java/com/luisantolin/daw/prog/App.java
+++ EjemploJdbc/src/main/java/com/luisantolin/daw/prog/App.java
@@ -6,10 +6,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/*
+ * CREACION DE LA BBDD, DEL USUARIO Y PERMISOS
+ * 
+ * create database testdb;
+ * CREATE USER 'lantolin'@'%' IDENTIFIED BY 'changeme';
+ * GRANT ALL PRIVILEGES ON testdb.* TO 'lantolin'@'%';
+ */
+
 public class App {
 	public static void main( String[] args ) {
 		try {
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "user", "changeme");
+			Connection con = DriverManager.getConnection("jdbc:mysql://lantolin.daw.practicas.net:3306/testdb?serverTimezone=UTC", "lantolin", "changeme");
 			Statement stmt = con.createStatement();
 //			ResultSet resultSet = stmt.executeQuery("SELECT * FROM prueba_tb");
 			ResultSet resultSet = stmt.executeQuery("SELECT sysdate() as fecha FROM dual");

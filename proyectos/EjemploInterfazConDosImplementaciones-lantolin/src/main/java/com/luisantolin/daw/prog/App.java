56fbc51d7d854075b279c852425c131bc49dc038
Luis Antolin <luis.antolin@gmail.com>
2023-05-19 15:21:10 +0200

    mejora documentación

--- proyectos/EjemploInterfazConDosImplementaciones-lantolin/src/main/java/com/luisantolin/daw/prog/App.java
+++ proyectos/EjemploInterfazConDosImplementaciones-lantolin/src/main/java/com/luisantolin/daw/prog/App.java
@@ -3,11 +3,17 @@ package com.luisantolin.daw.prog;
 import com.luisantolin.daw.debugger.Debugger;
 
 /**
+ * Ejemplo de una aplicacion que puede seleccionar que debugger usa.
+ * Acepta por línea de comandos el FQ-Name de la clase (1er argumento).
+ * 
+ * Lo importante de este ejemplo es que las clases que quieran "ser" un Debugger
+ * tienen que implementar el interfaz Debugger, así el código que las usa
+ * puede estar seguro de que la clase va a tener el método debug()
+ * 
  * Ejemplo de varias cosas: 
  * 1. un interfaz (Debugger) que tiene dos implementaciones 
- * 2. como construir una clase solo sabiendo su
- * Fully-Qualified-Name 
- * 3. como hacer "inyeccion" de codigo es decir que mi App use una clase u otra
+ * 2. como construir una clase solo sabiendo su Fully-Qualified-Name 
+ * 3. como hacer "inyeccion" de codigo es decir que mi App use una clase u otra solo con el nombre de la clase
  * 
  * @author lantolin
  */

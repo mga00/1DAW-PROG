db138ded4e6c0548f69b795ba01108bd3b0dbd71
Luis Antolin <luis.antolin@gmail.com>
2023-06-07 12:45:54 +0200

    añado codigo para controlar el orden de los tests

--- proyectos/ProyectoBase-lantolin/src/test/java/com/luisantolin/daw/prog/AppTest.java
+++ proyectos/ProyectoBase-lantolin/src/test/java/com/luisantolin/daw/prog/AppTest.java
@@ -1,12 +1,16 @@
 package com.luisantolin.daw.prog;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 
 /**
  * Unit test for simple App.
  */
+@TestMethodOrder( MethodOrderer.DisplayName.class )
 public class AppTest 
 {
     /**

74e790a283943abb6283c87f351a4b9fe3e16eff
Luis Antolin <luis.antolin@gmail.com>
2024-02-12 10:08:53 +0100

    quito configuración de clase main del exec:java
    hay un problema cuando hay multiples main() porque no se puede forzar
    desde command line si está configurado en el plugin

--- proyectos/ProyectoBase-lantolin/src/main/java/com/gsd/daw/prog/App.java
+++ proyectos/ProyectoBase-lantolin/src/main/java/com/gsd/daw/prog/App.java
@@ -9,13 +9,14 @@ package com.gsd.daw.prog;
  * <code>id1</code> tanto en el nombre del proyecto como en el
  * <code>pom.xml</code></li>
  * <li>La clase que contiene el metodo <code>main()</code> que lanza la
- * funcionalidad es <code>App</code>, eso esta configurado en pom.xml para
- * usarse tanto desde el JAR como desde <code>mvn exec:java</code></li>
+ * funcionalidad es <code>App</code>, eso esta configurado en
+ * <code>pom.xml</code> para ser la clase main del JAR</li>
  * </ol>
  */
 public class App {
     /**
      * Punto de entrada de la aplicación
+     * 
      * @param args No se espera ningún parámetro.
      */
     public static void main( String[] args ) {

e2725f2c42e82cee464ffc28d5606ef64225e94a
Luis Antolin <luis.antolin@gmail.com>
2024-02-19 00:30:13 +0100

    Mejora de reporting de tests.

--- proyectos/ProyectoBase-lantolin/src/test/java/com/gsd/daw/prog/AppTest.java
+++ proyectos/ProyectoBase-lantolin/src/test/java/com/gsd/daw/prog/AppTest.java
@@ -2,16 +2,16 @@ package com.gsd.daw.prog;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder( MethodOrderer.DisplayName.class )
-public class AppTest 
-{
+public class AppTest {
     @Test
-    public void testDeCobertura1()
-    {
+    @DisplayName( "Test de Cobertura1" )
+    public void testDeCobertura1() {
         // este test no hace nada, simplemente da un 100% de cobertura en los reportes.
         App myApp = new App();
         App.main( new String[] { "" } );

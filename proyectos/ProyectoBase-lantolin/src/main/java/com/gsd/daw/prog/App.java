9f8a7b557550b60ced9624ad23f130243dbf6b5b
Luis Antolin <luis.antolin@gmail.com>
2024-02-18 14:14:06 +0100

    Mejora para incorporar más Javadoc
    Incorpora el overview en el sitio estandar de maven y los package-info
    en el sitio estandar javadoc

--- proyectos/ProyectoBase-lantolin/src/main/java/com/gsd/daw/prog/App.java
+++ proyectos/ProyectoBase-lantolin/src/main/java/com/gsd/daw/prog/App.java
@@ -1,17 +1,7 @@
 package com.gsd.daw.prog;
 
 /**
- * Hello world! Proyecto para ser usado como ejemplo o base para un proyecto
- * Java. Características:
- * <ol>
- * <li>Maven, configurado para usar JUnit 5 y Java 17</li>
- * <li>El proyecto se llama ProyectoBase-id1, para usarlo tienes que poner tu
- * <code>id1</code> tanto en el nombre del proyecto como en el
- * <code>pom.xml</code></li>
- * <li>La clase que contiene el metodo <code>main()</code> que lanza la
- * funcionalidad es <code>App</code>, eso esta configurado en
- * <code>pom.xml</code> para ser la clase main del JAR</li>
- * </ol>
+ * Hola Mundo!
  */
 public class App {
     /**

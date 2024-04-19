a61eedeaa7672794c71e424c5fd815e5431d594a
Luis Antolin <luis.antolin@gmail.com>
2024-04-19 10:44:12 +0200

    Primer ejemplo de Spring Boot

---
+++ proyectos/EjemploSpringBoot03/src/main/java/com/example/demo/Book.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import java.util.List;
+import java.util.Objects;
+
+public record Book(String isbn, String title, List<String> authors) {
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Book book = (Book) o;
+		return Objects.equals(isbn, book.isbn);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(isbn);
+	}
+}

eb6217917d16ff5cf6893afaea52339188f1d140
Luis Antolin <luis.antolin@gmail.com>
2024-12-19 09:25:36 +0100

    commit para probar la action

--- proyectos/EjemploLeerFichero/src/main/java/com/luisantolin/daw/prog/App.java
+++ proyectos/EjemploLeerFichero/src/main/java/com/luisantolin/daw/prog/App.java
@@ -5,28 +5,27 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App {
-	public static void main( String[] args ) {
+	public static void main(String[] args) {
 		if (args.length < 1) {
 			System.err.println("USO: <comando> <nombre-de-fichero>");
 			return;
 		}
 
-		Scanner inputFile = null;
+		Scanner inputScannerFromFile = null;
+		File f = new File(args[0]);
 		try {
-			File f = new File(args[0]);
-			inputFile = new Scanner( f );
-
+			inputScannerFromFile = new Scanner(f);
 		} catch (FileNotFoundException e) {
-			System.err.println("ERROR: no se puede abrir el fichero [" + args[0] + "]");
+			System.err.println("ERROR: no se puede abrir [" + args[0] + "]\n" + e.getMessage());
 			return;
 		}
-		
-        while (inputFile.hasNext()) {
-            String line = inputFile.nextLine();
-            System.out.println("---" + line + "---");
-        }
 
-        inputFile.close();
+		while (inputScannerFromFile.hasNext()) {
+			String line = inputScannerFromFile.nextLine();
+			System.out.println("@@@" + line + "@@@");
+		}
+
+		inputScannerFromFile.close();
 
 	}
 

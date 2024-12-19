f12160023f6c0b9d4246e601796ffc2222a56b47
Luis Antolin <luis.antolin@gmail.com>
2024-12-19 09:26:53 +0100

    error intencionado para probar actions

--- proyectos/EjemploLeerFichero/src/main/java/com/luisantolin/daw/prog/App.java
+++ proyectos/EjemploLeerFichero/src/main/java/com/luisantolin/daw/prog/App.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class App {
+public class App2 {
 	public static void main(String[] args) {
 		if (args.length < 1) {
 			System.err.println("USO: <comando> <nombre-de-fichero>");

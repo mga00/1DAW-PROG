27a08f791c87802d72140046dd9ea8f9f4b91ae7
Luis Antolin <luis.antolin@gmail.com>
2024-12-21 08:08:23 +0100

    corrección del error intencionado

--- proyectos/EjemploLeerFichero/src/main/java/com/luisantolin/daw/prog/App.java
+++ proyectos/EjemploLeerFichero/src/main/java/com/luisantolin/daw/prog/App.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class App2 {
+public class App {
 	public static void main(String[] args) {
 		if (args.length < 1) {
 			System.err.println("USO: <comando> <nombre-de-fichero>");

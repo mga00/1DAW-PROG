da1dc3193099571a366bad750e92c5b46c5ee3ab
m.gonzalez <m.gonzalez@2DAW-04.BUINF.local>
2025-03-17 09:50:28 +0100

    Inicio Analizer

--- ApacheLogDataMining-mgonzalez/src/main/java/com/gsd/daw/prog/ApacheAnalizer/App.java
+++ ApacheLogDataMining-mgonzalez/src/main/java/com/gsd/daw/prog/ApacheAnalizer/App.java
@@ -1,5 +1,15 @@
 package com.gsd.daw.prog.ApacheAnalizer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gsd.daw.prog.ApacheLogLoader.CompArgs;
+
 public class App {
 	
 	public static void main(String[] args) {
@@ -18,8 +28,36 @@ public class App {
 			return;
 		}
 	    // Creacion de la conexión
-	    System.out.println( "INFO: conectado a BBDD." );
+		String ip=args[0];
+		if (!CompArgs.compIp(ip)) {
+			System.err.println("Error: La ip no es correcta");
+			return;
+		}
+		String nombre=args[1];
+		
+		String url="jdbc:oracle:thin:@//"+ip+"/"+nombre;
+	    String user=args[2];
+	    
+	    String contra=args[3];
+	    String query= "Select * from apache_log_tbl";
 	    // Carga de objetos del modelo de BBDD a estructura plana
+	    List<String[]> logsBbdd = new ArrayList<>();
+	    try (Connection conn = DriverManager.getConnection(url, user, contra)) {
+	    	Statement stmt = conn.createStatement();
+	    	ResultSet rs = stmt.executeQuery(query);
+	    	int contador= rs.getMetaData().getColumnCount();
+	    	while (rs.next()) {
+	    		String[] fila = new String[contador];
+	    		for (int i = 1; i <= contador; i++) {
+					fila[i-1] = rs.getString(i);
+				}
+	    		logsBbdd.add(fila);
+	    	}
+		} catch (SQLException e) {
+			System.err.println("ERROR: excepción SQL: " + e.getMessage());
+			return;
+		}
+		System.out.println( "INFO: conectado a BBDD." );
 	    // Esto sin colecciones será un String[][] array de tamaño máximo 40000
 	    // elementos
 	    // Crea una clase aparte cuya responsabilidad sea recibir una conexion de BBDD 
